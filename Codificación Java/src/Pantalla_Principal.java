43e1ec32c460df71b3bd232938ff56d2f52c6b1d
Gerardo <51869601+genrique14@users.noreply.github.com>
2019-11-20 20:40:18 -0500

    Pantalla_Principal.java
    Realicé cambios en los nombres de algunos métodos y añadí (si mal no recuerdo) una variable.
    Iré realizando los cambios en los otros .java para evitar errores...

--- Codificación Java/src/Pantalla_Principal.java
+++ Codificación Java/src/Pantalla_Principal.java
@@ -1,54 +1,59 @@
 import java.util.Scanner;
 public class Pantalla_Principal {
   // Declaracion de variables
-public String usuarios[];
-public int puntaje[];
-public String pregunta[];
-public String respuesta[];
-public String opciones[];
-
-
-
-  public static void  imprimirPantalla(){
-    System.out.println("1. Configuracion");
-    System.out.println("2. Juegos");
-    System.out.println("3. Consulta de Puntajes Obtenidos");
-    System.out.println("4. Salir");
-  }
-
-  // lee la respuesta del usuario y procede a seleccionarmenu
-  public static void leerRespuesta() {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Inserte una opción: ");
-    String respuesta = sc.nextLine();
-    seleccionarMenu(respuesta);
-  }
-  // Es la funcion principal que permite escoger al usuario la opcion
-  public static void seleccionarMenu(String respuesta){
-    switch (respuesta){ // Necesito hacer que esta funcion se itere
-      case "1":
-        Configuracion config = new Configuracion(); // Crea el objeto de la clase segun la opcion
-        config.imprimirPantalla();
-        break;
-      case "2":
-        Juego juego = new Juego();
-        juego.imprimirHola();
-        break;
-      case "3":
-        Consulta_Puntajes consulta = new Consulta_Puntajes();
-        consulta.imprimirPantalla();
-        break;
-      case "4":
-        System.out.println("Hasta luego ");
-        break;
+    public String usuarios[];
+    public int puntaje[];
+    public String pregunta[];
+    public String respuesta[];
+    public String opciones[];
+    
+    public static void ImprimirPantalla(){
+        System.out.println("1. Configuracion");
+        System.out.println("2. Juegos");
+        System.out.println("3. Consulta de Puntajes Obtenidos");
+        System.out.println("4. Salir");
     }
-  }
+
+    // lee la respuesta del usuario y procede a seleccionarmenu
+    public static int LeerRespuesta() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("\nInserte una opción: ");
+        int respuesta = sc.nextInt();
+        return respuesta;
+    }
+
+    public static void EscogerRespuesta(int respuesta) {
+        while(respuesta != 4) {
+            switch (respuesta) {
+                case 1:
+                    Configuracion config = new Configuracion(); // Crea el objeto de la clase segun la opcion
+                    config.MostrarOpciones();
+                    break;
+                case 2:
+                    Juego juego = new Juego();
+                    juego.imprimirHola();
+                    break;
+                case 3:
+                    Consulta_Puntajes consulta = new Consulta_Puntajes();
+                    consulta.imprimirPantalla();
+                    break;
+            }
+            ImprimirPantalla();
+            respuesta = LeerRespuesta();
+        }
+        System.out.println("Hasta luego, gracias por jugar");
+    
+    }
 
 
-  public static void main(String[] args) {
-    imprimirPantalla();
-    leerRespuesta();
-
+    public static void main(String[] args) {
+        //Declaracion de variables
+        Scanner sc = new Scanner(System.in);
+        int opcionPrincipal;
+        //Llamado a los metodos
+        ImprimirPantalla();
+        opcionPrincipal =  LeerRespuesta();
+        EscogerRespuesta(opcionPrincipal);
 
   }
 }
