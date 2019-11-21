eea9c4890afb71f110af87a3f3a0fdd860b312ae
Gerardo <51869601+genrique14@users.noreply.github.com>
2019-11-20 20:41:40 -0500

    Configuracion.java
    Cambié el nombre del método para que resultara un poco más claro y evitar confusiones

--- Codificación Java/src/Configuracion.java
+++ Codificación Java/src/Configuracion.java
@@ -1,6 +1,6 @@
 public class Configuracion {
 
-  public void imprimirPantalla(){
+  public void MostrarOpciones(){
     System.out.println("Ingrese la pregunta");
     System.out.println("Ingrese las posibles respuestas correctas");
     System.out.println("Ingrese la respuesta correcta");
